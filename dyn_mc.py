0817cef8b09359b8dab90df3a5a27790b1e34904
Brod8362 <brod8362@gmail.com>
2022-06-12 11:08:52 -0500

    Initial commit?

--- dyn_mc.py
+++ dyn_mc.py
@@ -1,5 +1,15 @@
 #!/bin/python3
 import os
+import socket
+import ctypes
+from ctypes import byref
+
+mclib = ctypes.cdll.LoadLibrary("./mcvars.so")
+mclib.read_var_int.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_uint64)]
+mclib.read_var_int.restype = ctypes.c_int32
+mclib.read_var_long.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_uint64)]
+mclib.read_var_long.restype = ctypes.c_int64
+
 
 def main():
     if not os.path.exists("server.properties"):
@@ -17,15 +27,35 @@ def main():
                 server_properties
                 server_properties[args[0]] = args[1]
 
-    if not server_properties.get("rcon.port", ""):
+    if "server-port" not in server_properties:
+        print("server port not specified")
+        os._exit(1)
+
+    SERVER_PORT = int(server_properties["server-port"])
+
+    if "rcon.port" not in server_properties:
         print("rcon port not specified")
         os._exit(1)
 
-    if not server_properties.get("enable-rcon", "false") == "true":
+    RCON_PORT = int(server_properties["rcon.port"])
+
+    if server_properties.get("enable-rcon", "false") == "false":
         print("rcon is not enabled")
         os._exit(1)
 
-    
+    sock = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM
+    )
+    sock.bind(("127.0.0.1", SERVER_PORT))
+    sock.listen(2)
+    while True:
+        conn, addr = sock.accept()
+        data = conn.recv(1024)
+        pos = ctypes.c_ulong(0)
+        length = mclib.read_var_int(data, byref(pos))
+        print(f"RAWLEN: {len(data)} DLEN: {length} POS:{pos}")
+        print(f"{addr}: {data}")
 
+    
 if __name__ == "__main__":
     main()
